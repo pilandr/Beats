c64ceb9c0a450b104ad746e2d972b8e1b39731d1
Andrei Piliushenko <pionbox@gmail.com>
2021-05-22 00:05:45 +0300

    add vertical accordion

--- js/accordion-vertical.js
+++ js/accordion-vertical.js
@@ -1,47 +1,31 @@
-// const accordionArrow = document.querySelector(".team__list");
-// accordionArrow.addEventListener("click", e => {
-
-//   const ishumanArrow = e.target.classList.contains("human__arrow");
-  
-//   // console.log(e.target);
-//   if (ishumanArrow)
-//   {
-    
-//     const profBlockWrapper = e.target.closest(".human").querySelector(".human__profession-block-wrapper");
-//     const profBlock = e.target.closest(".human").querySelector(".human__profession-block");
-//     profBlockWrapper.style.height = window.getComputedStyle(profBlock,null).height;
-    
-//     if (e.target.closest(".human__name-wrap").classList.contains("human__name-wrap--active")) {
-//       profBlockWrapper.style.height = 0;
-//       e.target.closest(".human__name-wrap").classList.remove("human__name-wrap--active");
-//     } else e.target.closest(".human__name-wrap").classList.add("human__name-wrap--active");
-//   }
-// });
-
-
-// $(document).ready((e) => {
-
-//   const teamList = $('.team__list');
-//   teamList.on("click", e => {
-    
-//     console.log($(e.target));
-//     console.log(e.target);
-//   });
-
-// });
 const openItem = item => {
   const container = item.closest(".human");
   const contentBlock = container.find(".human__profession-block-wrapper");
   const textBlock = contentBlock.find(".human__profession-block");
-  const reqHeight = textBlock.Height();
-
-  contentBlock.Height(reqHeight);
+  const reqHeight = textBlock.height();
+  item.addClass("human__name-wrap--active");
+  contentBlock.height(reqHeight);
 }
+
+
+const closeEveryItem = item => {
+  const container = item.closest(".team__list");
+  const items = container.find(".human__profession-block-wrapper");
+  const itemsContainer = container.find(".human__name-wrap");
+  itemsContainer.removeClass("human__name-wrap--active");
+  items.height(0);
+}
+
 
 $(".human__name-wrap").on("click", (e) => {
   $this = $(e.currentTarget);
-  $this.toggleClass("human__name-wrap--active");
-  openItem($this);
+  
+  if ($this.hasClass("human__name-wrap--active")) {
+    closeEveryItem($this);
+  } else {
+    closeEveryItem($this);
+    openItem($this);
+  }
 });
 
 
