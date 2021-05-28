3ed9dad5ff8233d1fb59f8d3c840b75176479529
Andrei Piliushenko <pionbox@gmail.com>
2021-05-29 02:08:30 +0300

    add one page scroll and edit horisontal acordion

--- js/accordion-horisontal.js
+++ js/accordion-horisontal.js
@@ -1,17 +1,104 @@
-const links = document.querySelectorAll(".js-color-link");
+// const links = document.querySelectorAll(".js-color-link");
 
-links.forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    const currentLink = e.currentTarget;
-    const colorItem = currentLink.closest(".color");
-    if (colorItem.classList.contains("color--active")) {
-      colorItem.classList.remove("color--active");
+// links.forEach(link => {
+//   link.addEventListener("click", e => {
+//     e.preventDefault();
+//     const currentLink = e.currentTarget;
+//     const colorItem = currentLink.closest(".color");
+    
+//     const textPaddingLeft = parseInt(window.getComputedStyle(colorItem.querySelector(".color__content"), null).paddingLeft);
+//     const textPaddingRight = parseInt(window.getComputedStyle(colorItem.querySelector(".color__content"), null).paddingRight);
+//     const windowWidth = window.innerWidth;
+//     const isMobile = window.matchMedia("(max-width: 768px)").matches;
+//     console.log(isMobile); 
+//     let textWidth = 0;
+//     if (isMobile) {
+//       textWidth = windowWidth - textPaddingLeft - textPaddingRight - currentLink.clientWidth*3;
+//       console.log(windowWidth);
+//       console.log(textPaddingLeft);
+//       console.log(textPaddingRight);
+//       console.log(currentLink.clientWidth*3);
+
+//     } else {
+//       textWidth = 524 - textPaddingLeft - textPaddingRight;
+//     }
+    
+//     colorItem.querySelector(".color__content-inner").style.width = textWidth + "px";
+//     console.log(colorItem.querySelector(".color__content-inner").clientWidth);
+
+//     if (colorItem.classList.contains("color--active")) {
+//       colorItem.classList.remove("color--active");
+//     } else {
+//       document.querySelectorAll(".color--active").forEach(item => {
+//         item.classList.remove("color--active");
+//       });
+      
+//       colorItem.classList.add("color--active");
+      
+//     }
+//     console.log();
+//   });
+// });
+(function() {
+
+  const mesureWidth = item => {
+    let reqItemWidth = 0;
+    const screenWidth = $(window).width();
+    container = item.closest(".colors");
+    const items = container.find(".colors__header");
+    const itemsWidth = items.width() * items.length;
+
+    const textContainer = item.find(".colors__content-inner");
+    const textPaddingLeft = parseInt(textContainer.css("padding-left"));
+    const textPaddingRight = parseInt(textContainer.css("padding-right"));
+
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+    if (isMobile) {
+      reqItemWidth = screenWidth - itemsWidth;
     } else {
-      document.querySelectorAll(".color--active").forEach(item => {
-        item.classList.remove("color--active");
-      });
-      colorItem.classList.add("color--active");
+      reqItemWidth = 524;
+    }
+
+    return {
+      container: reqItemWidth,
+      textContainer: reqItemWidth - textPaddingLeft - textPaddingRight
+    }
+  }
+  
+  const closeEveryItemInContainer = container => {
+    const items = container.find(".colors__item");
+    const contents = container.find(".colors__content");
+    items.removeClass("active");
+    contents.width(0);
+  }
+  
+  const openItem = item => {
+    const hiddenContent = item.find(".colors__content");
+    const reqWidth = mesureWidth(item);
+    const textBlock = item.find(".colors__content-inner");
+    
+
+
+    item.addClass("active");
+    hiddenContent.width(reqWidth.container);
+    textBlock.width(reqWidth.textContainer);
+  }
+  
+  $(".colors__header").on("click", e => {
+    e.preventDefault();
+    const $this = $(e.currentTarget);
+    const item = $this.closest(".colors__item");
+    const isOpenItem = item.hasClass("active");
+    const container = item.closest(".colors")
+
+    if (isOpenItem) {
+      closeEveryItemInContainer(container);
+    } else {
+      closeEveryItemInContainer(container);
+      openItem(item);
     }
   });
-});
+
+
+})()
